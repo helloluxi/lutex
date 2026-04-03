2dfb96071ea5a2823a4b41854c6941e71881bd08
Xi Lu <helloluxi@outlook.com>
2026-04-03 09:53:06 +0800

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-npm run package && npm run compile:cli && code --install-extension lutex-ext-0.0.1.vsix && windsurf --install-extension lutex-ext-0.0.1.vsix
+npm run package
+npm run compile:cli
+code --install-extension lutex-ext-0.0.1.vsix
+windsurf --install-extension lutex-ext-0.0.1.vsix
+cursor --install-extension lutex-ext-0.0.1.vsix
